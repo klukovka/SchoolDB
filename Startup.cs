2987b484c31cae2d9d4cafc6053ab271f3a7a3f8
klukovka <58118015+klukovka@users.noreply.github.com>
2021-02-15 15:23:40 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolDB.Startup))]
+namespace SchoolDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
